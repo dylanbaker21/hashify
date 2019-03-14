54f212e93a1d87b6c3d17a6c419b22b1517cbecf
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-03-14 16:01:39 -0400

    cors

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,9 +3,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Data = require("./data");
+const cors = require("cors");
 
 const API_PORT = 3001;
 const app = express();
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const router = express.Router();
 
 // this is our MongoDB database
@@ -24,14 +37,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
