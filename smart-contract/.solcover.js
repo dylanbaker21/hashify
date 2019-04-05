f558b838d8fe22d58ae78443b02b54b5fb31f04e
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-04-05 04:33:03 -0400

    add specific accounts for sig verify unit tests

---
+++ smart-contract/.solcover.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port: 8555,
+  testrpcOptions:
+    '-p 8555 --account="0x0e5d954264db5912029fe87c7ddb7ca62f347a680d72b0e0fb4271b4792c3394",0x9990000000000000000000 --account="0x0123456789012345678901234567890123456789012345678901234567890123",0x9990000000000000000000'
+  //testCommand: 'mocha --timeout 5000',
+  //norpc: true
+  //dir: './secretDirectory',
+  //copyPackages: ['openzeppelin-solidity'],
+  //skipFiles: ['Routers/EtherRouter.sol']
+};
