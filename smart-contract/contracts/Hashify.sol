99df94d1982158ceb4c9cfe5a93334c323b755e5
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-04-05 04:45:39 -0400

    updated to 0.2.1 contract

--- smart-contract/contracts/Hashify.sol
+++ smart-contract/contracts/Hashify.sol
@@ -1,11 +1,9 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.0;
 
 contract Hashify {
 
     address public owner;
-    
-    mapping(address => mapping(address => bool)) allowed;
-    mapping(address => hashInfo[]) hashes;
+    mapping(address => hashInfo[]) internal hashes;
     
     struct hashInfo {
         bytes32 hash;
@@ -17,12 +15,9 @@ contract Hashify {
     }
     
     event HashAdded(bytes32 indexed hash, uint256 indexed index, address indexed account);
-    event VerificationRegistered(bytes32 indexed hash, uint256 indexed block, address indexed account);
-    event VerificationChecked(address indexed account, bytes32 indexed hash, bool indexed verified);
-    event HashesGotten(bytes32[] indexed hashes, address indexed account);
-    event HashInfoGotten(bytes32 hash, bool verified, uint256 block, uint8 v, bytes32 r,bytes32 s);
-    event Allowed(address indexed account, address indexed allowee);
-    event Disallowed(address indexed account, address indexed disallowee);
+    event HashVerified(bytes32 indexed hash, uint256 indexed block, address indexed account);
+    event HashesListed(bytes32[] hashes, address indexed account);
+    event HashInfo(bytes32 indexed hash, bool indexed verified, uint256 indexed block, uint8 v, bytes32 r,bytes32 s);
     event HashDeleted(address indexed account, bytes32 indexed hash);
 
     constructor() public {
@@ -39,27 +34,19 @@ contract Hashify {
         bytes32 prefixedHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hashToVerify));
         require(msg.sender == ecrecover(prefixedHash, v, r, s));
         hashes[msg.sender][_index] = hashInfo(hashToVerify,true,block.number,v,r,s);
-        emit VerificationRegistered(hashes[msg.sender][_index].hash, block.number, msg.sender);
-    }
-
-    function checkVerification(address _address, uint256 _index) public returns(bool isVerified) {
-        require(allowed[_address][msg.sender] || msg.sender == _address);
-        emit VerificationChecked(_address, hashes[_address][_index].hash, hashes[_address][_index].signed);
-        return hashes[_address][_index].signed;
+        emit HashVerified(hashes[msg.sender][_index].hash, block.number, msg.sender);
     }
     
-    function getHashes() public returns(bytes32[] memory hashArray) {
+    function getHashes() public {
         bytes32[] memory returnHashes = new bytes32[](hashes[msg.sender].length);
         for (uint i = 0; i<hashes[msg.sender].length; i++){
             returnHashes[i] = hashes[msg.sender][i].hash;
         }
-        emit HashesGotten(returnHashes, msg.sender);
-        return returnHashes;
+        emit HashesListed(returnHashes, msg.sender);
     }
     
     function getHashInfo(address _address, uint256 _index) public {
-        require(allowed[_address][msg.sender] || msg.sender == _address);
-        emit HashInfoGotten(hashes[_address][_index].hash,
+        emit HashInfo(hashes[_address][_index].hash,
         hashes[_address][_index].signed,
         hashes[_address][_index].signedAtBlock,
         hashes[_address][_index].v,
@@ -67,27 +54,13 @@ contract Hashify {
         hashes[_address][_index].s);
     }
     
-    function allow(address _address) public returns(address) {
-        allowed[msg.sender][_address] = true;
-        emit Allowed(msg.sender, _address);
-        return _address;
-    }
-    
-    function disAllow(address _address) public returns(address) {
-        allowed[msg.sender][_address] = false;
-        emit Disallowed(msg.sender, _address);
-        return _address;
-    }
-    
-    function deleteHash(uint256 _index) public returns(bytes32 deletedHash) 
-    {
+    function deleteHash(uint256 _index) public {
         require(_index < hashes[msg.sender].length);
-        deletedHash = hashes[msg.sender][_index].hash;
+        bytes32 deletedHash = hashes[msg.sender][_index].hash;
         for (uint i = _index; i<hashes[msg.sender].length-1; i++){
             hashes[msg.sender][i] = hashes[msg.sender][i+1];
         }
         hashes[msg.sender].length--;
         emit HashDeleted(msg.sender, deletedHash);
-        return deletedHash;
     }
 }
