24b683dd3a7ceec8352e4ef060b395fff851e4c8
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-04-13 04:10:30 -0400

    add login authentication

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Data = require("./data");
 const cors = require("cors");
+const ethUtil = require("ethereumjs-util");
+const jwt = require("jsonwebtoken");
 
 const DB_CRED = require("./db");
 
@@ -54,6 +56,16 @@ router.get("/getData", (req, res) => {
   });
 });
 
+// fetches ONE USERS DATA
+router.get("/getData:publicAddress", (req, res) => {
+  let pubAddy = req.params.publicAddress;
+  console.log(pubAddy);
+  db.collection("users").findOne({ publicAddress: pubAddy }, (err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json(data);
+  });
+});
+
 // overwrites existing data in our database
 router.post("/updateData", (req, res) => {
   let id1 = req.body.id;
@@ -96,30 +108,125 @@ router.delete("/deleteData:id", (req, res) => {
 });
 
 // adds new data to the database
+// ADD NEW USER
 router.post("/putData", (req, res) => {
   let data = new Data();
+  const publicAddress = req.body.publicAddress;
+  console.log(publicAddress);
 
-  const { id, fromAddress, hash, tx } = req.body.newHashItem;
+  // if ((!id && id !== 0) || !hash) {
+  //   return res.json({
+  //     success: false,
+  //     error: "INVALID INPUTS"
+  //   });
+  // }
 
-  if ((!id && id !== 0) || !hash) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
-    });
-  }
-
-  data.hash = hash;
-  data.id = id;
-  data.fromAddress = fromAddress;
-  data.tx = tx;
+  data.publicAddress = publicAddress;
+  //data.hashes.push({ id, hash, tx: null });
   data.save(err => {
     if (err) return res.json({ success: false, error: err });
-    Data.find((err, data) => {
-      if (err) return res.json({ success: false, error: err });
-      return res.json(data);
-    });
+    return res.json(data);
+    // Data.find((err, data) => {
+    //   if (err) return res.json({ success: false, error: err });
+    //   return res.json(data);
+    // });
   });
 });
+
+// AUTHENTICATE USER
+router.post("/auth", (req, res) => {
+  const pubAddy = req.body.publicAddress;
+  const signature = req.body.signature;
+
+  console.log(pubAddy + " " + signature);
+
+  // if ((!id && id !== 0) || !hash) {
+  //   return res.json({
+  //     success: false,
+  //     error: "INVALID INPUTS"
+  //   });
+  // }
+  db.collection("users").findOneAndUpdate(
+    { publicAddress: pubAddy },
+    { $set: { nonce: Math.floor(Math.random() * 1000000) } },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        return res.json({ success: false, error: err });
+      } else {
+        const msg = `Login key: ${data.value.nonce}`;
+        console.log(data.value.nonce);
+        // We now are in possession of msg, publicAddress and signature. We
+        // can perform an elliptic curve signature verification with ecrecover
+        const msgBuffer = ethUtil.toBuffer(msg);
+        const msgHash = ethUtil.hashPersonalMessage(msgBuffer);
+        const signatureBuffer = ethUtil.toBuffer(signature);
+        const signatureParams = ethUtil.fromRpcSig(signatureBuffer);
+        const publicKey = ethUtil.ecrecover(
+          msgHash,
+          signatureParams.v,
+          signatureParams.r,
+          signatureParams.s
+        );
+        const addressBuffer = ethUtil.publicToAddress(publicKey);
+        const address = ethUtil.bufferToHex(addressBuffer);
+        console.log(address + " " + data.value.publicAddress);
+
+        // dataSchema.nonce = Math.floor(Math.random() * 10000);
+        if (err) return res.json({ success: false, error: err });
+        if (address === data.value.publicAddress) {
+          jwt.sign(
+            { payload: { id: data.value._id, address } },
+            DB_CRED.JWT_SECRET,
+            (err, token) => {
+              if (err) {
+                return res.json(err);
+              }
+              return res.json(token);
+            }
+          );
+        } else {
+          return res
+            .status(401)
+            .send({ error: "Signature verification failed" });
+        }
+        // The signature verification is successful if the address found with
+        // ecrecover matches the initial publicAddress
+      }
+    }
+  );
+});
+
+router.post("/test", verifyToken, (req, res) => {
+  jwt.verify(req.token, DB_CRED.JWT_SECRET, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "SUCCESS :)",
+        authData
+      });
+    }
+  });
+});
+
+function verifyToken(req, res, next) {
+  // get auth header value
+  const bearerHeader = req.headers["authorization"];
+
+  if (typeof bearerHeader !== "undefined") {
+    //split token at space
+    const bearer = bearerHeader.split(" ");
+    //get token from array
+    const bearerToken = bearer[1];
+    //set token
+    req.token = bearerToken;
+    //next middleware
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 // append /api for our http requests
 app.use("/api", router);
