09518de958d3991d3040b6b1d8aece8a162279d0
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-04-13 04:09:34 -0400

    new db schema for logins

--- back-end/data.js
+++ back-end/data.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// database's data structure
+// hash storage schema
 const DataSchema = new Schema(
   {
     id: String,
-    fromAddress: String,
     hash: String,
     tx: String
   },
   { timestamps: true }
 );
 
+// user schema
+const UserSchema = new Schema(
+  {
+    publicAddress: { type: String, index: true, unique: true },
+    nonce: { type: String, default: Math.floor(Math.random() * 1000000) },
+    hashes: [DataSchema]
+  },
+  { timestamps: true }
+);
+
 // export for use elsewhere
-module.exports = mongoose.model("Data", DataSchema);
+module.exports = mongoose.model("User", UserSchema);
