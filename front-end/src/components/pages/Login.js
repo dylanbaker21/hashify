b50cf3ec8e3b9633818748d530a30935adf14e7d
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-04-16 17:33:48 -0400

    add EIP-1102 support (metamask privacy mode)

--- front-end/src/components/pages/Login.js
+++ front-end/src/components/pages/Login.js
@@ -1,51 +1,85 @@
 import React, { Component } from "react";
-import "./Login.css";
 import axios from "axios";
 
-class Login extends Component {
-  handleClick = async e => {
-    const publicAddress = window.web3.eth.coinbase;
-    let netId = await window.web3.version.network;
+import MetaMaskPic from "../../public/download-metamask-dark.png";
+import "./Login.css";
 
-    // if no account found tell user to login to metamask
-    if (publicAddress == null) {
+class Login extends Component {
+  handleClick = async () => {
+    // Modern dapp browsers
+    if (window.ethereum) {
+      try {
+        // Request account access
+        await window.ethereum.enable();
+        // Acccounts now exposed
+        const publicAddress = window.ethereum.selectedAddress;
+        const netId = await window.ethereum.networkVersion;
+
+        // If Ropsten is not the active network
+        if (netId !== "3") {
+          window.alert(
+            "You must switch to the Ropsten test network in MetaMask"
+          );
+        }
+        // Check the database for the user's address and send return data to handleRegistration
+        else {
+          axios
+            .get(`http://localhost:3001/api/getData${publicAddress}`)
+            .then(res => this.handleRegistration(res, publicAddress));
+        }
+      } catch (error) {
+        // User denied account access
+        console.log(error);
+      }
+    }
+    // Legacy dapp browsers
+    else if (window.web3) {
       window.alert(
-        'You must login to the chrome extension "metamask" to interact with the blockchain'
+        "Please update MetaMask or use a dApp browser compatible with EIP-1102"
       );
-    } else if (netId !== "3") {
-      window.alert("You must select the Ropsten test network in metamask");
-    } else {
-      axios
-        .get(`http://localhost:3001/api/getData${publicAddress}`)
-        .then(res => this.handleRegistration(res, publicAddress));
+    }
+    // Non-dapp browsers
+    else {
+      let willDownload = await window.confirm(
+        'The browser extension "MetaMask" is needed to use this app. Click "OK" to download.'
+      );
+      if (willDownload) {
+        window.open("https://www.metamask.io", "_blank");
+      }
     }
   };
 
   handleRegistration = async (res, publicAddress) => {
-    // if account didn't exist in db, create one
+    // If account doesn't exist in database, create one then handleLogin
     if (res.data === null) {
       axios
         .post(`http://localhost:3001/api/putData`, { publicAddress })
         .then(res => this.handleLogin(res.data.publicAddress, res.data.nonce));
-    } else if (res.data.publicAddress === publicAddress) {
+    }
+    // If account does exist handleLogin
+    else if (res.data.publicAddress === publicAddress) {
       this.handleLogin(res.data.publicAddress, res.data.nonce);
     }
   };
 
   handleLogin = async (publicAddress, nonce) => {
+    // Sign login message
     window.web3.personal.sign(
       window.web3.fromUtf8(`Login key: ${nonce}`),
       publicAddress,
       (err, signature) => {
         if (err) {
-          console.log(err);
-        } else {
+          console.log("User Denied Message Signature");
+        }
+        // Send signature to backend for verification and authentication
+        else {
           axios
             .post(`http://localhost:3001/api/auth`, {
               publicAddress,
               signature
             })
-            .then(res => localStorage.setItem("token", res.data)); //TODO: protect routes and use token on them, refactor code to work with new login, comment code, git commits
+            .then(res => localStorage.setItem("token", res.data)) // Set JWT auth token to local storage
+            .then(res => (window.location.href = "/home")); // Open the home page
         }
       }
     );
@@ -55,6 +89,13 @@ class Login extends Component {
     return (
       <React.Fragment>
         <div className="pageContainer">
+          <a href={"https://www.metamask.io"} target={"_blank"}>
+            <img
+              src={MetaMaskPic}
+              alt={"Click here to download metamask"}
+              className={"metamask"}
+            />
+          </a>
           <div className="content">
             <h1>Hashify</h1>
             <button className="loginButton" onClick={this.handleClick}>
